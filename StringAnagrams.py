fdf32937b3a30e4578c7d5702827a8c85c57a37d
Akshada-26 <130067077+Akshada-26@users.noreply.github.com>
2023-10-23 23:35:06 +0530

    StringAnagrams.py

---
+++ StringAnagrams.py
@@ -0,0 +1,15 @@
+def are_anagrams(str1, str2):
+    # Remove spaces and convert to lowercase for case-insensitive comparison
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
+
+    # Check if the sorted characters of both strings are the same
+    return sorted(str1) == sorted(str2)
+
+# Test the function
+string1 = "listen"
+string2 = "silent"
+if are_anagrams(string1, string2):
+    print(f"{string1} and {string2} are anagrams.")
+else:
+    print(f"{string1} and {string2} are not anagrams.")
