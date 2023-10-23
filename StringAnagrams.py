7f1e9b8859d53f2546b49f43f6efe88041ce89fe
Akshada-26 <130067077+Akshada-26@users.noreply.github.com>
2023-10-23 23:36:06 +0530

    Coding question

--- StringAnagrams.py
+++ StringAnagrams.py
@@ -3,7 +3,7 @@ def are_anagrams(str1, str2):
     str1 = str1.replace(" ", "").lower()
     str2 = str2.replace(" ", "").lower()
 
-    # Check if the sorted characters of both strings are the same
+    
     return sorted(str1) == sorted(str2)
 
 # Test the function
